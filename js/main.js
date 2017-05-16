fc0f201825105fa13b272bfdb3465bbaa64b61c9
jperaino <jprn72@gmail.com>
2017-05-16 14:56:42 -0400

    add grid to scene

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     // MARK: - PROPERTIES ----------------------------------------------------------------------------
 
-    	// SET HYPERBOLOID VARIABLES
+    	// SET Base VARIABLES
 
 		// Tubes
 		var cylRad = 0.15;
@@ -26,27 +26,36 @@ $(document).ready(function(){
 
 		// Materials
 		var material = new THREE.MeshBasicMaterial({color: 0xffffff, transparent: true});
-		var material2 = new THREE.MeshBasicMaterial({color: 0x4d2b90});
-		material.opacity = 1;
+		var lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 
 
     	// SET SCENE - - - - - - - 
     	var scene = new THREE.Scene();
-		scene.background = new THREE.Color( 0x9474cc );
+		scene.background = new THREE.Color( 0xb2dfdb );
 
 		var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-		camera.position.set(circ2pos/2, -80 , cylHeight/2);
+		camera.position.set(-60, -60 , 80);
 		camera.up = new THREE.Vector3(0,0,1);
-		camera.lookAt(new THREE.Vector3(circ2pos/2, 0 , cylHeight/2));
-
+		camera.lookAt(new THREE.Vector3(0, 0 , 0));
 
 		var renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setSize( window.innerWidth, window.innerHeight);
 
 		controls = new THREE.OrbitControls(camera, renderer.domElement);
-		controls.target.set(circ2pos/2, 0 , cylHeight/2)
-		controls.enableZoom = false;
+		controls.target.set(0, 0 , 0);
+		//controls.enableZoom = false;
 
+
+		// GRID
+		// GRID XY static
+		var size = 10000;
+		var divisions = 1000;
+
+		var grid = new THREE.GridHelper( size, divisions, 0xffffff, 0xffffff );
+		grid.rotation.x = Math.PI/2;
+		grid.material.transparent = true;
+		grid.material.opacity = 0.4;
+		scene.add( grid );
 
 		
 
@@ -55,113 +64,29 @@ $(document).ready(function(){
 		// Add WebGL scene to HTML
 		$('#canvasPlaceholder').html( renderer.domElement );
 
-		buildHyperboloid();
 
 		// Add date to copyright 
 		var d = new Date()
 		var year = d.getFullYear()
 		$('#crDate').html( "<i>Copyright &#169  " + year + " Jim Peraino. All rights reserved.</i>");
 
+		// RENDER
+		addSiteOutline();
+		render();
 		
 	// MARK: - ADD GEOMETRY  ----------------------------------------------------------------------------
-		 function updateHyperboloid() {
-		 	var hyperOrig = scene.getObjectByName("hyperboloid");
-		 	var stairEndsOrig = scene.getObjectByName("stairEnds")
 
-			// Transform top circle
-			top.rotation.z = circ2Rot;
-			top.position.x = circ2pos;
+		function addSiteOutline() {
 
-			scene.updateMatrixWorld();
-	        top.updateMatrixWorld();		
+			var geometry = new THREE.Geometry();
+			geometry.vertices.push(new THREE.Vector3(-10,0,0));
+			geometry.vertices.push(new THREE.Vector3(0,10,0));
+			geometry.vertices.push(new THREE.Vector3(10,0,0));
+			geometry.vertices.push(new THREE.Vector3(-10,0,0));
 
-			// Update cylinders
-			for(i=0; i < circPtCt; i++) {
-				var cyl = hyperOrig.children[0].children[i];
+			var line = new THREE.Line(geometry, lineMaterial);
 
-				// get target point
-				var focalPt = new THREE.Vector3();
-				focalPt.setFromMatrixPosition( top.children[i].matrixWorld );
-
-				// Reorient cylinder to target point
-				cyl.lookAt(focalPt);
-
-				// Scale cylinder to target point
-				var scale = cyl.position.distanceTo(focalPt)/cylHeight;
-				cyl.scale.z = scale;
-
-				// Move stair end point
-				var stairEndOrig = stairEndsOrig.children[i];
-				var newPt = getPointInBetweenByLength(cyl.position, focalPt, stairLength);
-				stairEndOrig.position.x = newPt.x;
-				stairEndOrig.position.y = newPt.y;
-				stairEndOrig.position.z = newPt.z;
-			}
-		}
-
-
-		function buildHyperboloid() {
-			
-			// Generate points on top and bottom circles
-			for(i=0; i < circPtCt; i++) {
-				
-				// Find start points
-				var degrees = ((360/circPtCt)*i);
-				var radians = degrees * Math.PI / 180;
-
-				var posX = Math.cos(radians) * circRad;
-				var posY = Math.sin(radians) * circRad;
-
-				// Build cylinder
-				var geometry = new THREE.CylinderBufferGeometry(cylRad, cylRad, cylHeight);
-				var cylinder = new THREE.Mesh(geometry, material);
-				cylinder.geometry.rotateX( Math.PI / 2);
-				cylinder.geometry.translate(0,0, cylHeight/2 );
-				
-				// Move cylinder to position
-				cylinder.position.x = posX;
-				cylinder.position.y = posY;
-
-				var topPointPoint = new THREE.SphereBufferGeometry(.5);
-				var topPoint = new THREE.Mesh(topPointPoint, material);
-				topPoint.position.x = posX;
-				topPoint.position.y = posY;
-				topPoint.position.z = cylHeight;
-
-				cylinder.lookAt(topPoint.position);
-
-				// Add stair end point
-	       		var stairGeo = new THREE.SphereBufferGeometry(.5);
-	       		var endSphere = new THREE.Mesh(stairGeo, material2);
-	       		endSphere.position.x = posX;
-	       		endSphere.position.y = posY; 
-
-	       		stairEnds.add(endSphere);
-
-				top.add(topPoint);
-				cylinders.add (cylinder);
-			}
-
-	       	hyperboloid.add( cylinders );
-	       	hyperboloid.name = "hyperboloid";
-
-	       	stairEnds.name = "stairEnds";
-
-	       	// Add geometries to scene
-			scene.add( hyperboloid );
-			scene.add( stairEnds );
-
-			updateHyperboloid();
-			
-			// render the scene
-			render();
-		};
-	
-
-		function buildStair() {
-
-			
-			
+			scene.add(line);
 		}
 
 	// MARK: - EVENT LISTENERS --------------------------------------------------------------------
@@ -182,12 +107,6 @@ $(document).ready(function(){
 		}
 
 		// With JQuery
-		// $('#ex1').slider({
-		// 	formatter: function(value) {
-		// 		return 'Current value: ' + value;
-		// 	}
-		// });
-
 		$('#ex2').slider({
 			formatter: function(value) {
 				return 'Current value: ' + value;
@@ -206,10 +125,6 @@ $(document).ready(function(){
 			}
 		});
 
-		// $('#ex1').on("slide", function(slideEvt){
-		// 	cylHeight = slideEvt.value;
-		// 	updateHyperboloid();
-		// })
 
 		$('#ex2').on("slide", function(slideEvt2){
 			rotation = slideEvt2.value * Math.PI / 180;
@@ -243,13 +158,6 @@ $(document).ready(function(){
 		};
 
 	// ___GEOMETRY HELPERS
-		// Return stair endpoint location
-		function getPointInBetweenByLength(pointA, pointB, length) {
-			var dir = pointB.clone().sub(pointA).normalize().multiplyScalar(length);
-			finalPt = pointA.clone().add(dir);
-
-			return finalPt;
-		}
 
 
 	// ___ALERTS
